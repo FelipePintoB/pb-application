ba57819143b39a9f5c3848870a7badc7f2ea3a39
Felipe Pinto <afpinto@zaito.com.co>
2022-05-23 21:10:12 -0500

    Commit inicial del proyecto

---
+++ src/app/galeries/galeries.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GaleriesRoutingModule } from './galeries-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GaleriesRoutingModule
+  ]
+})
+export class GaleriesModule { }
---
+++ src/app/google-maps/google-maps.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GoogleMapsRoutingModule } from './google-maps-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GoogleMapsRoutingModule
+  ]
+})
+export class GoogleMapsModule { }
